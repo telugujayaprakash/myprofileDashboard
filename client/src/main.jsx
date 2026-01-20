4ed4f3c9e90127759a4eda33d9eb90958750ea7a
telugujayaprakash <jayaprakash96030@gmail.com>
2026-01-20 11:24:17 +0530

    fixed analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
-import { Analytics } from '@vercel/analytics/next'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <Analytics>
-        <App />
-      </Analytics>
+      <App />
+      <Analytics />
     </Provider>
   </StrictMode>
 )
